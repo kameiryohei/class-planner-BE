89380fa4a2fcea65acbf401f557eb0614d5612ca
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-09-06 10:52:29 +0900

    planのmodelを定義

---
+++ model/plan.go
@@ -0,0 +1,17 @@
+package model
+
+import "time"
+
+type Plan struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title" gorm:"not null"`
+	Content   *string   `json:"content"`
+	UserID    uint      `json:"user_id" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+
+	User      User           `json:"user" gorm:"foreignKey:UserID"`
+	Courses   []Course       `json:"courses" gorm:"foreignKey:PlanID"`
+	Posts     []Post         `json:"posts" gorm:"foreignKey:PlanID"`
+	Favorites []FavoritePlan `json:"favorites" gorm:"foreignKey:PlanID"`
+}
