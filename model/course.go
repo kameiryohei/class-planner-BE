abe555514ba4384f0eb30cdd0a2462fd0c082db9
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-09-11 22:39:42 +0900

    タイムスタンプが必要ない箇所を削除

--- model/course.go
+++ model/course.go
@@ -3,13 +3,12 @@ package model
 import "time"
 
 type Course struct {
-	ID        uint       `json:"id" gorm:"primaryKey"`
-	Name      string     `json:"name" gorm:"not null"`
-	Content   *string    `json:"content"`
-	PlanID    uint       `json:"plan_id" gorm:"not null"`
-	CreatedAt time.Time  `json:"created_at"`
-	UpdatedAt time.Time  `json:"updated_at"`
-	DeletedAt *time.Time `json:"deleted_at" gorm:"index"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"not null"`
+	Content   *string   `json:"content"`
+	PlanID    uint      `json:"plan_id" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 
 	Plan Plan `json:"plan" gorm:"foreignKey:PlanID"`
 }
