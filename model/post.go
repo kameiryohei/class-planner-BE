b430ed643ab1f527f643d6b78135362461dff980
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-10-31 22:10:34 +0900

    postの返り値の返り値のmodelを変更

--- model/post.go
+++ model/post.go
@@ -3,10 +3,15 @@ package model
 type Post struct {
 	ID        uint    `json:"id" gorm:"primaryKey"`
 	Content   *string `json:"content"`
-	Published bool    `json:"published" gorm:"default:false"`
 	AuthorID  *uint   `json:"author_id"`
 	PlanID    *uint   `json:"plan_id"`
+	CreatedAt *string `json:"created_at"`
 
 	Author User  `json:"author" gorm:"foreignKey:AuthorID"`
 	Plan   *Plan `json:"plan" gorm:"foreignKey:PlanID"`
+}
+type PostResponse struct {
+	ID        uint    `json:"id" gorm:"primaryKey"`
+	Content   *string `json:"content"`
+	CreatedAt *string `json:"created_at"`
 }
