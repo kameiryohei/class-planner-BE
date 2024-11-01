aacdfc76ba5a73c08d6231543db49532b40e46f8
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-01 17:25:48 +0900

    postのrepository層のフォーマット修正

--- repository/post_repository.go
+++ repository/post_repository.go
@@ -8,9 +8,9 @@ import (
 )
 
 type IPostRepository interface {
-	GetAllPosts(posts *[]model.Post,userId uint)  error//ユーザーが作成した全ての投稿を取得
-	GetPostByID(post *model.Post, id uint,postId uint) error
-	CreatePost(post model.Post)error
+	GetAllPosts(posts *[]model.Post, userId uint) error //ユーザーが作成した全ての投稿を取得
+	GetPostByID(post *model.Post, id uint, postId uint) error
+	CreatePost(post model.Post) error
 	DeletePostByID(id uint) error
 }
 
@@ -22,29 +22,33 @@ func NewPostRepository(db *gorm.DB) IPostRepository {
 	return &postRepository{db}
 }
 
-func (pr *postRepository) GetAllPosts(posts *[]model.Post,userId uint) error {
+// すべての投稿を取得
+func (pr *postRepository) GetAllPosts(posts *[]model.Post, userId uint) error {
 	if err := pr.db.Joins("User").Where("author_id = ?", userId).Order("created_at").Find(posts).Error; err != nil {
 		return err
 	}
 	return nil
-} 
+}
 
-func (pr *postRepository) GetPostByID(post *model.Post,courseId uint,postId uint)error{
-	if err := pr.db.Joins("Plan").Where("id = ?", courseId).First(post,postId).Error; err != nil {
+// 投稿IDで投稿を取得
+func (pr *postRepository) GetPostByID(post *model.Post, courseId uint, postId uint) error {
+	if err := pr.db.Joins("Plan").Where("id = ?", courseId).First(post, postId).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
+// 投稿を作成
 func (pr *postRepository) CreatePost(post model.Post) error {
 	if err := pr.db.Create(post).Error; err != nil {
-		return err 
+		return err
 	}
 	return nil
 }
 
-func (pr *postRepository) DeletePostByID(id uint) error {
-	result := pr.db.Where("id = ?", id).Delete(&model.Post{})
+// 投稿を削除
+func (pr *postRepository) DeletePostByID(postId uint) error {
+	result := pr.db.Where("id = ?", postId).Delete(&model.Post{})
 	if result.Error != nil {
 		return result.Error
 	}
