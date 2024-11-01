8c63fe8cea46db48bb6399f44168732b127445ee
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-01 21:57:28 +0900

    AuthorIdの型を修正

--- model/post.go
+++ model/post.go
@@ -3,12 +3,12 @@ package model
 type Post struct {
 	ID        uint    `json:"id" gorm:"primaryKey"`
 	Content   *string `json:"content"`
-	AuthorID  *uint   `json:"author_id"`
 	PlanID    *uint   `json:"plan_id"`
 	CreatedAt *string `json:"created_at"`
 
-	Author User  `json:"author" gorm:"foreignKey:AuthorID"`
-	Plan   *Plan `json:"plan" gorm:"foreignKey:PlanID"`
+	Author   User  `json:"author" gorm:"foreignKey:AuthorID"`
+	AuthorID uint  `json:"author_id"`
+	Plan     *Plan `json:"plan" gorm:"foreignKey:PlanID"`
 }
 type PostResponse struct {
 	ID        uint    `json:"id" gorm:"primaryKey"`
