9f3d5e70939558d11631a7e6927b2bca8af1c11a
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-01 22:29:57 +0900

    postのルーティングを追加

--- router/router.go
+++ router/router.go
@@ -2,14 +2,26 @@ package router
 
 import (
 	"backend/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter (uc controller.IUserController) *echo.Echo {
-	e:= echo.New()
+func NewRouter(uc controller.IUserController, pc controller.IPostController) *echo.Echo {
+	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.LogIn)
 	e.POST("/logout", uc.Logout)
+	t := e.Group("/posts")
+	// middlewareを追加
+	t.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey:  []byte(os.Getenv("SECRET")),
+		TokenLookup: "cookie:token",
+	}))
+	t.GET("", pc.GetAllPosts)
+	t.GET("/:postId", pc.GetPostByID)
+	t.POST("", pc.CreatePost)
+	t.DELETE("/:postId", pc.DeletePostByID)
 	return e
 }
