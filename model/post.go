3863b6dee01bcb7fdae6a5b0f7ba3ca07408f1de
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-06 17:34:39 +0900

    CreatedAtをTime型に変更

--- model/post.go
+++ model/post.go
@@ -1,17 +1,18 @@
 package model
 
-type Post struct {
-	ID        uint    `json:"id" gorm:"primaryKey"`
-	Content   *string `json:"content"`
-	PlanID    *uint   `json:"plan_id"`
-	CreatedAt *string `json:"created_at"`
+import "time"
 
-	Author   User  `json:"author" gorm:"foreignKey:AuthorID"`
-	AuthorID uint  `json:"author_id"`
-	Plan     *Plan `json:"plan" gorm:"foreignKey:PlanID"`
+type Post struct {
+	ID        uint       `json:"id" gorm:"primaryKey;autoIncrement"`
+	Content   *string    `json:"content"`
+	PlanID    *uint      `json:"plan_id"`
+	CreatedAt *time.Time `json:"created_at" gorm:"autoCreateTime"`
+	Author    User       `json:"author" gorm:"foreignKey:AuthorID"`
+	AuthorID  uint       `json:"author_id"`
+	Plan      *Plan      `json:"plan" gorm:"foreignKey:PlanID"`
 }
 type PostResponse struct {
-	ID        uint    `json:"id" gorm:"primaryKey"`
-	Content   *string `json:"content"`
-	CreatedAt *string `json:"created_at"`
+	ID        uint       `json:"id" gorm:"primaryKey"`
+	Content   *string    `json:"content"`
+	CreatedAt *time.Time `json:"created_at"`
 }
