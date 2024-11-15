d64436c7b48d979b1b7c293e99e36576e478be18
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-15 23:14:44 +0900

    planに関するusecase を作成

---
+++ usecase/plan_usecase.go
@@ -0,0 +1,102 @@
+package usecase
+
+import (
+	"backend/model"
+	"backend/repository"
+)
+
+type IPlanUsecase interface {
+	GetAllPlans() ([]model.PlanResponse, error)
+	GetPlanByID(planId uint) (model.PlanDetailResponse, error)
+	CreatePlan(plan *model.Plan) (model.PlanResponse, error)
+	UpdatePlan(plan *model.Plan) (model.PlanResponse, error)
+	DeletePlanByID(planId uint) error
+}
+
+type planUsecase struct {
+	pr repository.IPlanRepository
+}
+
+func NewPlanUsecase(pr repository.IPlanRepository) IPlanUsecase {
+	return &planUsecase{pr}
+}
+
+func (pu *planUsecase) GetAllPlans() ([]model.PlanResponse, error) {
+	plans := []model.Plan{}
+	if err := pu.pr.GetAllPlans(&plans); err != nil {
+		return nil, err
+	}
+	resPlans := []model.PlanResponse{}
+	for _, v := range plans {
+		p := model.PlanResponse{
+			ID:        v.ID,
+			Title:     v.Title,
+			Content:   v.Content,
+			UserID:    v.UserID,
+			CreatedAt: v.CreatedAt,
+			UpdatedAt: v.UpdatedAt,
+			User:      v.User,
+		}
+		resPlans = append(resPlans, p)
+	}
+	return resPlans, nil
+}
+
+func (pu *planUsecase) GetPlanByID(planId uint) (model.PlanDetailResponse, error) {
+	plan := model.PlanDetailResponse{}
+	if err := pu.pr.GetPlanByID(&plan, planId); err != nil {
+		return model.PlanDetailResponse{}, err
+	}
+	resPlan := model.PlanDetailResponse{
+		ID:        plan.ID,
+		Title:     plan.Title,
+		Content:   plan.Content,
+		UserID:    plan.UserID,
+		CreatedAt: plan.CreatedAt,
+		UpdatedAt: plan.UpdatedAt,
+		User:      plan.User,
+		Courses:   plan.Courses,
+		Posts:     plan.Posts,
+		Favorites: plan.Favorites,
+	}
+	return resPlan, nil
+}
+
+func (pu *planUsecase) CreatePlan(plan *model.Plan) (model.PlanResponse, error) {
+	if err := pu.pr.CreatePlan(plan); err != nil {
+		return model.PlanResponse{}, err
+	}
+	resPlan := model.PlanResponse{
+		ID:        plan.ID,
+		Title:     plan.Title,
+		Content:   plan.Content,
+		UserID:    plan.UserID,
+		CreatedAt: plan.CreatedAt,
+		UpdatedAt: plan.UpdatedAt,
+		User:      plan.User,
+	}
+	return resPlan, nil
+}
+
+func (pu *planUsecase) UpdatePlan(plan *model.Plan) (model.PlanResponse, error) {
+	if err := pu.pr.UpdatePlan(plan); err != nil {
+		return model.PlanResponse{}, err
+	}
+	resPlan := model.PlanResponse{
+		ID:        plan.ID,
+		Title:     plan.Title,
+		Content:   plan.Content,
+		UserID:    plan.UserID,
+		CreatedAt: plan.CreatedAt,
+		UpdatedAt: plan.UpdatedAt,
+		User:      plan.User,
+	}
+	return resPlan, nil
+}
+
+func (pu *planUsecase) DeletePlanByID(planId uint) error {
+	if err := pu.pr.DeletePlanByID(planId); err != nil {
+		return err
+	}
+	return nil
+}
