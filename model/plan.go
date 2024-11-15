f6e4a0a2630dd6922c47a59f73b909b4cb735977
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-15 23:15:23 +0900

    レスポンスのモデルを定義

--- model/plan.go
+++ model/plan.go
@@ -15,3 +15,26 @@ type Plan struct {
 	Posts     []Post         `json:"posts" gorm:"foreignKey:PlanID"`
 	Favorites []FavoritePlan `json:"favorites" gorm:"foreignKey:PlanID"`
 }
+
+type PlanResponse struct {
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Title     string    `json:"title"`
+	Content   *string   `json:"content"`
+	UserID    uint      `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	User      User      `json:"user"`
+}
+
+type PlanDetailResponse struct {
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	Title     string         `json:"title"`
+	Content   *string        `json:"content"`
+	UserID    uint           `json:"user_id"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	User      User           `json:"user"`
+	Courses   []Course       `json:"courses"`
+	Posts     []Post         `json:"posts"`
+	Favorites []FavoritePlan `json:"favorites"`
+}
