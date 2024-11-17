c636f07a1ac9b6f62ac685d1391e919428b793a5
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-17 23:17:04 +0900

    userとcorsesのmodelを変更

--- model/plan.go
+++ model/plan.go
@@ -27,14 +27,14 @@ type PlanResponse struct {
 }
 
 type PlanDetailResponse struct {
-	ID        uint           `json:"id" gorm:"primaryKey"`
-	Title     string         `json:"title"`
-	Content   *string        `json:"content"`
-	UserID    uint           `json:"user_id"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	User      User           `json:"user"`
-	Courses   []Course       `json:"courses" gorm:"foreignKey:PlanID"`
-	Posts     []Post         `json:"posts" gorm:"foreignKey:PlanID"`
-	Favorites []FavoritePlan `json:"favorites" gorm:"foreignKey:PlanID"`
+	ID        uint             `json:"id" gorm:"primaryKey"`
+	Title     string           `json:"title"`
+	Content   *string          `json:"content"`
+	UserID    uint             `json:"user_id"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	User      UserResponse     `json:"user"`
+	Courses   []CourseResponse `json:"courses" gorm:"foreignKey:PlanID"`
+	Posts     []Post           `json:"posts" gorm:"foreignKey:PlanID"`
+	Favorites []FavoritePlan   `json:"favorites" gorm:"foreignKey:PlanID"`
 }
