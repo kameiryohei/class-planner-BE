beba7f434ffa045d3b3cf11cb462527270ef65fe
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-18 22:40:01 +0900

    双方向リレーションを削除

--- model/university.go
+++ model/university.go
@@ -4,6 +4,5 @@ type University struct {
 	ID   uint   `json:"id" gorm:"primaryKey"`
 	Name string `json:"name" gorm:"not null"`
 
-	Users     []User    `json:"users" gorm:"foreignKey:UniversityID"`
 	Faculties []Faculty `json:"faculties" gorm:"foreignKey:UniversityID"`
 }
