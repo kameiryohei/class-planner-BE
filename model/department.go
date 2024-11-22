c48fe4de02ee6e0a2a0cacebb9d654ce7a44adc7
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-11-22 11:37:47 +0900

    responseで返すmodelを作成

--- model/department.go
+++ model/department.go
@@ -1,10 +1,11 @@
 package model
 
 type Department struct {
-	ID        uint   `json:"id" gorm:"primaryKey"`
-	Name      string `json:"name" gorm:"not null"`
-	FacultyID uint   `json:"faculty_id" gorm:"not null"`
+	ID   uint   `json:"id" gorm:"primaryKey"`
+	Name string `json:"name" gorm:"not null"`
+}
 
-	Faculty Faculty `json:"faculty" gorm:"foreignKey:FacultyID"`
-	Users   []User  `json:"users" gorm:"foreignKey:DepartmentID"`
+type DepartmentResponse struct {
+	ID   uint   `json:"id" gorm:"primaryKey"`
+	Name string `json:"name"`
 }
