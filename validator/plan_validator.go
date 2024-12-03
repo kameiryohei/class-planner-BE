5c2443f0e8838c060b8b479d941baa4cc2f25973
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-12-03 12:48:42 +0900

    planに関するvalidatorを作成

---
+++ validator/plan_validator.go
@@ -0,0 +1,32 @@
+package validator
+
+import (
+	"backend/model"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type IPlanValidator interface {
+	PlanValidate(plan model.Plan) error
+}
+
+type PlanValidator struct{}
+
+func NewPlanValidator() IPlanValidator {
+	return &PlanValidator{}
+}
+
+func (plv *PlanValidator) PlanValidate(plan model.Plan) error {
+	return validation.ValidateStruct(&plan,
+		validation.Field(
+			&plan.Title,
+			validation.Required.Error("Title is required"),
+			validation.Length(1, 20).Error("limited max 20 characters"),
+		),
+		validation.Field(
+			&plan.Content,
+			validation.Required.Error("Content is required"),
+			validation.Length(1, 50).Error("limited max 50 characters"),
+		),
+	)
+}
