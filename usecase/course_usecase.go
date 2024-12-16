4cdf8a6906bed298bb8b11e01ef4b6da339be024
Ryohei Kamei <231205751@ccmailg.meijo-u.ac.jp>
2024-12-16 23:11:30 +0900

    courseのusecaseを作成

---
+++ usecase/course_usecase.go
@@ -0,0 +1,59 @@
+package usecase
+
+import (
+	"backend/model"
+	"backend/repository"
+)
+
+type ICourseUsecase interface {
+	GetAllCourses(planId uint) ([]model.CourseResponse, error)
+	CreateCourse(course *[]model.Course) (model.CourseResponse, error)
+	UpdateCourse(course *model.Course, courseId int) (model.CourseResponse, error)
+	DeleteCourseByID(courseId uint) error
+}
+
+type courseUsecase struct {
+	cr repository.ICourseRepository
+}
+
+func NewCourseUsecase(cr repository.ICourseRepository) ICourseUsecase {
+	return &courseUsecase{cr}
+}
+
+func (cu *courseUsecase) GetAllCourses(planId uint) ([]model.CourseResponse, error) {
+	courses := []model.Course{}
+	if err := cu.cr.GetAllCourses(&courses, planId); err != nil {
+		return nil, err
+	}
+	resCourses := []model.CourseResponse{}
+	for _, v := range courses {
+		c := model.CourseResponse{
+			ID:      v.ID,
+			Name:    v.Name,
+			Content: v.Content,
+		}
+		resCourses = append(resCourses, c)
+	}
+	return resCourses, nil
+}
+
+func (cu *courseUsecase) CreateCourse(course *[]model.Course) (model.CourseResponse, error) {
+	if err := cu.cr.CreateCourse(course); err != nil {
+		return model.CourseResponse{}, err
+	}
+	return model.CourseResponse{}, nil
+}
+
+func (cu *courseUsecase) UpdateCourse(course *model.Course, courseId int) (model.CourseResponse, error) {
+	if err := cu.cr.UpdateCourse(course, courseId); err != nil {
+		return model.CourseResponse{}, err
+	}
+	return model.CourseResponse{}, nil
+}
+
+func (cu *courseUsecase) DeleteCourseByID(courseId uint) error {
+	if err := cu.cr.DeleteCourseByID(courseId); err != nil {
+		return err
+	}
+	return nil
+}
